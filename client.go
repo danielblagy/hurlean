89aa055083b6f37d555ff7edb0d4e1ac032a5358
Daniel Blagy <daniel.blagy@gmail.com>
2021-06-19 19:29:28 +0300

    created a client test app module

--- client.go
+++ client.go
@@ -1,1 +1,18 @@
 package hurlean
+
+
+import (
+	"net"
+	"strconv"
+	"fmt"
+)
+
+
+func ConnectToServer(ip string, port int) {
+	conn, err := net.Dial("tcp", ip + ":" + strconv.Itoa(port))
+	if err != nil {
+		fmt.Println("Failed to connect to the server", err)
+	}
+	
+	fmt.Println("Successfully connected to the server", conn)
+}
---
+++ test_app_client/main.go
@@ -0,0 +1,13 @@
+// this file is not a part of the library, it's ised to test the execution
+
+package main
+
+
+import (
+	"github.com/danielblagy/hurlean"
+)
+
+
+func main() {
+	hurlean.ConnectToServer("localhost", 8080)
+}
