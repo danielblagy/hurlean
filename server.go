ee5f790d028ce18c3f329c25957eee0309de4d97
Daniel Blagy <daniel.blagy@gmail.com>
2021-07-19 18:43:50 +0300

    testing something out

--- server.go
+++ server.go
@@ -13,6 +13,10 @@ import (
 	"io"
 )
 
+func Hello() {
+	fmt.Println("This is github version test")
+}
+
 
 // When the server is started with hurlean.StartServer function call,
 // ServerInstance object will be created on success.
